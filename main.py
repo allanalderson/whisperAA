2714aaa271128866b0f7d0a0a413174f22cfeddd
Allan Alderson <adsaalderson@gmail.com>
2023-06-28 17:51:25 +1000

    attempt with CUDA

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import whisper
-# import os
-import tiktoken
+
 import torch
+import time
+import whisper
+import os
 
+audio = whisper.load_audio("audioFiles/allanVoice.mp3")
+device = torch.device('cuda')
+# model = whisper.load_model("small")
+model = whisper.load_model("small", device=device)
 #
-# import os
-#
-# # Get the base directory of your project
-# base_dir = os.path.dirname(os.path.abspath(__file__))
-# # Specify the relative path to the audio file
-# audio_file_path = os.path.join(base_dir, "audioFiles", "allanVoice")
-# print (audio_file_path)
-#
-# model = whisper.load_model("base")  #  model = whisper.load_model("base", device="cpu")
-#
-# # with torch.cuda.device(device):
-# result = model.transcribe(audio_file_path)
-#
-# print(result["text"])
+
+result = model.transcribe(audio)
 
 
-model = whisper.load_model("base")
-result = model.transcribe("audioFiles/allanVoice.mp3")
+
 print(result["text"])
