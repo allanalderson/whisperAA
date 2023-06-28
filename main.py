f86e2c893b2c52e45e575839af36877a570b7421
Allan Alderson <adsaalderson@gmail.com>
2023-06-28 18:32:03 +1000

    Now writing to an output file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import torch
 import whisper
+import sys
 
 if torch.cuda.is_available():
 	print("Current CUDA device:", torch.cuda.get_device_name())
@@ -15,11 +16,17 @@ else:
 	device = device = torch.device('cpu')
 
 
-audio = whisper.load_audio("audioFiles/allanVoice.mp3")
+audio = whisper.load_audio("inputAudio/AustralianVoice.mp3")
 model = whisper.load_model("small", device=device)
-
 result = model.transcribe(audio, language='en')
 
 
 
-print(result["text"])
+# print(result["text"])
+
+
+with open("outputText/output.txt", "w") as file:  # # Open a file for writing
+	sys.stdout = file  # Redirect the standard output to the file
+	print(result["text"])  # # Print the result to the file
+
+sys.stdout = sys.__stdout__  # # Restore the standard output
