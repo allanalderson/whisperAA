cfedff59eb444bb5d059b4e0b3dcce90e1747edc
Allan Alderson <adsaalderson@gmail.com>
2023-06-28 18:07:39 +1000

    Working with CUDA

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 
 import torch
-import time
 import whisper
-import os
+
+if torch.cuda.is_available():
+	print("Current CUDA device:", torch.cuda.get_device_name())
+	cuda_version = torch.version.cuda
+	print("CUDA Toolkit version:", cuda_version)
+	torch_version = torch.__version__
+	print("PyTorch version:", torch_version)
+	print()
+	device = device = torch.device('cuda')
+else:
+	print ("Torch GPU unavailable")
+	device = device = torch.device('cpu')
+
 
 audio = whisper.load_audio("audioFiles/allanVoice.mp3")
-device = torch.device('cuda')
-# model = whisper.load_model("small")
 model = whisper.load_model("small", device=device)
-#
 
 result = model.transcribe(audio)
 
